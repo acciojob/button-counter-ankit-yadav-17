5ee30e8cd20dc848ca31adf7787906c0b64dd9f9
Ankit Yadav <arao65212@gmail.com>
2023-07-12 10:39:22 +0530

    submitting code-Wed Jul 12 2023 10:39:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <p>Button clicked {count} times</p>
+        <p>Button clicked {count} times </p>
         <button onClick={() => setCount(count+1)}>Click Me</button>
     </div>
   )
