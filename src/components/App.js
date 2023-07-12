fbcce6e24f6e7f79be25c1bcffb90fb2dce67bdf
Ankit Yadav <arao65212@gmail.com>
2023-07-12 10:49:08 +0530

    submitting code-Wed Jul 12 2023 10:49:07 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import './../styles/App.css';
 
 const App = () => {
 
-  let [count,setcount] = useState[0];
+  let [count,setCount] = useState[0];
 
   return (
     <div>
         {/* Do not remove the main div */}
-        <p>Button clicked {count} times </p>
+        <p>Button clicked {count} times</p>
         <button onClick={() => setCount(count+1)}>Click Me</button>
     </div>
   )
